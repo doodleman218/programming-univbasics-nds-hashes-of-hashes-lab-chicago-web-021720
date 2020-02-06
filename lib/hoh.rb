8dd5045653d661d72313a1d17599cb96e1483a27
David Wolff <dcwolff218@gmail.com>
2020-02-06 18:38:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,7 +55,10 @@ def naming_system
     sub_category: {
         label: "Phylum",
         sub_category: {
-          label: "Class"  
+          label: "Class",
+          sub_category: {
+            label: "Order",
+          }
         }
       }
   }
