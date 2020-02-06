a75bf1a2458928811e5395bd7a509ac105ade43a
David Wolff <dcwolff218@gmail.com>
2020-02-06 18:39:16 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -58,6 +58,15 @@ def naming_system
           label: "Class",
           sub_category: {
             label: "Order",
+            sub_category: {
+              label: "Family",
+              sub_category: {
+                label: "Genus",
+                sub_category: {
+                  label: "Species"
+                }
+              }
+            }
           }
         }
       }
